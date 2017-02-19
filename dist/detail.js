7c58278b0e80b2ec64612c9fedae11e2741d5497
94dreamer <503633021@qq.com>
2017-02-19 23:48:33 +0800

    src

---
+++ dist/detail.js
@@ -0,0 +1,23 @@
+webpackJsonp([3,1],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(7);
+
+
+/***/ },
+
+/***/ 7:
+/***/ function(module, exports) {
+
+	'use strict';
+
+	/**
+	 * Created by zhouzhen on 2017/2/17.
+	 */
+	console.log('detail.js');
+
+/***/ }
+
+});
---
+++ dist/index.js
@@ -0,0 +1,31 @@
+webpackJsonp([2,1],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(8);
+
+
+/***/ },
+
+/***/ 3:
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ },
+
+/***/ 8:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	/**
+	 * Created by zhouzhen on 2017/2/17.
+	 */
+	__webpack_require__(3);
+	console.log('index.js');
+
+/***/ }
+
+});
